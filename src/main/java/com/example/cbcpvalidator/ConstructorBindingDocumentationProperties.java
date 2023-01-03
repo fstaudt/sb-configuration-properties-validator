a943a21ee08813af2562668ce954cb13188f8b4d
Francois Staudt <francois.staudt@soprabanking.com>
2023-01-03 09:36:18 +0100

    feat: reproduce validator issue on SB 3.0.0

--- src/main/java/com/example/cbcpvalidator/ConstructorBindingDocumentationProperties.java
+++ src/main/java/com/example/cbcpvalidator/ConstructorBindingDocumentationProperties.java
@@ -1,14 +1,12 @@
 package com.example.cbcpvalidator;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 import java.net.URL;
 
 @ConfigurationProperties("documentation")
-@ConstructorBinding
 public class ConstructorBindingDocumentationProperties implements Validator {
 
     private Boolean validated = false;
